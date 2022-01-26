232f38490bd820868e9ca827efc695df5b176ba7
Hatha Dam <hathadam9@gmail.com>
2022-01-25 23:43:12 -0800

    Delete server directory

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 5000 || process.env.PORT;
-
-app.use(express.static('client/dist'));
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-})
